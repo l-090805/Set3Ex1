239d8caf3794a042b688f764f230139cf364fe75
falau <falau@LENOVO>
2024-12-16 19:05:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Set3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu o seire de numere despartite prin spatii: ");
+            string input = Console.ReadLine();
+            int[] vector = Array.ConvertAll(input.Split(' '),int.Parse);
+            int sum = 0;
+            
+            for(int i = 0; i < vector.Length; i++)
+            {
+                sum += vector[i];
+            }
+            Console.WriteLine("Suma: " + sum);
+        }
+    }
+}
